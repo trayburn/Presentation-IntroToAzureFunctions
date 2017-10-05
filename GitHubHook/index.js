08fe7f6eccb1fff0af8cf782fe37d2ce5df41780
Tim Rayburn <tim@timrayburn.net>
2017-10-05 09:09:10 -0400

    syntax

--- GitHubHook/index.js
+++ GitHubHook/index.js
@@ -1,8 +1,8 @@
-module.exports = function (context, p) {
+module.exports = function (context, req) {
     context.log('Node.js GitHub WebHook function processed a request. RequestUri=%s', req.originalUrl);
 
     context.bindings.message = {
-        body : `The card ${p.project_card.note} has been ${p.action} by ${p.sender.login}`,
+        body : `The card ${req.project_card.note} has been ${req.action} by ${req.sender.login}`,
         to : "+18177600002"
     };
 
