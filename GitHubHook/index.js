213779c9ba2f3e6081a2ae138505b7615f40b2c2
Tim Rayburn <tim@timrayburn.net>
2017-10-05 09:07:09 -0400

    not just http request

--- GitHubHook/index.js
+++ GitHubHook/index.js
@@ -1,7 +1,6 @@
-module.exports = function (context, req) {
+module.exports = function (context, p) {
     context.log('Node.js GitHub WebHook function processed a request. RequestUri=%s', req.originalUrl);
 
-    var p = req.body;
     context.bindings.message = {
         body : `The card ${p.project_card.note} has been ${p.action} by ${p.sender.login}`,
         to : "+18177600002"
