09eb32845f5c1c50ab77154a31863e8ea4f833e4
Tim Rayburn <tim@timrayburn.net>
2017-10-05 09:03:26 -0400

    contextual message

--- GitHubHook/index.js
+++ GitHubHook/index.js
@@ -1,8 +1,9 @@
 module.exports = function (context, req) {
     context.log('Node.js GitHub WebHook function processed a request. RequestUri=%s', req.originalUrl);
 
+    var p = req.body;
     context.bindings.message = {
-        body : "ABC123",
+        body : `The card ${p.project_card.note} has been ${p.action} by ${p.sender.login}`,
         to : "+18177600002"
     };
 
