0f9016b4b25226aba4bf64002096a16989eff351
Tim Rayburn <tim@timrayburn.net>
2017-10-05 08:50:05 -0400

    updated code

--- GitHubHook/index.js
+++ GitHubHook/index.js
@@ -2,8 +2,6 @@ module.exports = function (context, req) {
     context.log('Node.js GitHub WebHook function processed a request. RequestUri=%s', req.originalUrl);
 
     context.bindings.message = {
-        accountSid : process.env.accountSid,
-        authToken : process.env.authToken,
         body : "ABC123",
         to : "+18177600002"
     };
