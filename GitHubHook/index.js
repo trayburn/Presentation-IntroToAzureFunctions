cd767637a0823d171626d7daae2343eb70bd0ce1
Tim Rayburn <tim@timrayburn.net>
2017-10-05 09:18:45 -0400

    terse

--- GitHubHook/index.js
+++ GitHubHook/index.js
@@ -1,6 +1,4 @@
 module.exports = function (context, req) {
-    context.log('Node.js GitHub WebHook function processed a request. RequestUri=%s', req.originalUrl);
-
     context.bindings.message = {
         body : `The card ${req.project_card.note} has been ${req.action} by ${req.sender.login}`,
         to : "+18177600002"
