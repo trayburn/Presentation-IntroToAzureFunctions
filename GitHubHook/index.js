cdb788070767a5d85ca85dcfe01e2e181580d2d3
Tim Rayburn <tim@timrayburn.net>
2017-10-05 13:48:50 -0400

    Updated phone

--- GitHubHook/index.js
+++ GitHubHook/index.js
@@ -1,7 +1,7 @@
 module.exports = function (context, req) {
     context.bindings.message = {
         body : `The card ${req.project_card.note} has been ${req.action} by ${req.sender.login}`,
-        to : "+18177600002"
+        to : "+16142096008"
     };
 
     context.done();
